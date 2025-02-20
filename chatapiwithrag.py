2a4a1cf86685300dba5f61134badc712c2247527
Tien Nguyen Trung <tien.nguyentrung@groovetechnology.com>
2025-02-20 19:27:14 +0700

    update data, update API using local LLM

--- chatapiwithrag.py
+++ chatapiwithrag.py
@@ -8,15 +8,15 @@ from langchain_community.vectorstores import FAISS
 import uvicorn
 
 # Initialize client
-#client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1") #ollama
-client = OpenAI()
+client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1") #ollama
+#client = OpenAI(api_key="lmstudio", base_url="http://localhost:1234/v1") #LM Studio
+#client = OpenAI()
 
-#embeddings = OllamaEmbeddings(model = "llama3.2")
-embeddings = OpenAIEmbeddings()
+embeddings = OllamaEmbeddings(model = "nomic-embed-text")
+#embeddings = OpenAIEmbeddings()
 
 
 app = FastAPI()
-vectorstore = FAISS.load_local("pf_index", embeddings, allow_dangerous_deserialization=True);
 
 class ChatRequest(BaseModel):
     message: str
@@ -29,8 +29,11 @@ async def index():
         loader = DirectoryLoader("data", glob="*.txt")
         documents = loader.load()
 
+        # In this example, each file content is a chunk
+        # but we can use variaty types of splitter to split large file to smaller chunks
+        # https://python.langchain.com/v0.1/docs/modules/data_connection/document_transformers/
         
-        # We can use FAISS (Facebook AI Similarity Search) or ChromaDB or InMemoryVectorStore for vector store
+        # We can use FAISS (Facebook AI Similarity Search) or ChromaDB or InMemoryVectorStore ... for vector storerage
         new_vectorstore = FAISS.from_documents(documents, embedding=embeddings)
 
         # Save the FAISS index for later use
@@ -43,13 +46,15 @@ async def index():
 @app.post("/chat")
 async def chat(request: ChatRequest):
     try:
+        vectorstore = FAISS.load_local("pf_index", embeddings, allow_dangerous_deserialization=True);
         # Retrieve relevant context
         context_docs = vectorstore.similarity_search(request.message, k=3)
         context_text = "\n".join([doc.page_content for doc in context_docs])
 
         # Generate response with RAG
         response = client.chat.completions.create(
-            model="gpt-4o-mini", #
+            #model="gpt-4o-mini", 
+            model="llama3.2", 
             messages=[
                 {"role": "system", "content": "Use the following context to answer the user query: " + context_text},
                 {"role": "user", "content": request.message}
